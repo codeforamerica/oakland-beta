efe7eb9790f32a45ee9e5f5229454b6ab8f63705
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2015-12-15 16:50:04 -0800

    adding env variables

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ switch ($_SERVER['SERVER_NAME'])
     // that identifies this environment that we can 
     // use in our multi-environment config
 
-    case 'heroku.com' :
+    case 'oakland-beta.heroku.com' :
         define('CRAFT_ENVIRONMENT', 'live');
         break;
 

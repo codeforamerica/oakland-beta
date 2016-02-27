ac5034cc28a2a8f24db8110eab817af81dbe13b8
Norris Hung <norrishung@gmail.com>
2016-02-26 17:31:17 -0800

    adding officials and initiatives content types

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
         },
         watch: {
           scripts: {
-            files: ['dev/sass/*'],
+            files: ['dev/sass/*', 'dev/sass/**/*'],
             tasks: 'sass',
             options: {
               spawn: false,

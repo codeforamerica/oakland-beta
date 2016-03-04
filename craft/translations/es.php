416cc397c0fb7393f626640494e69a73b780ef92
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2016-03-04 15:51:53 -0800

    added static spanish translations

--- craft/translations/es.php
+++ craft/translations/es.php
@@ -1,7 +1,87 @@
 <?php
 
 return array(
-	"Welcome to Oakland" => "La Bienvenida a Oakland"
+	## General
+	"Oakland" => "Oakland",
+	"City of Oakland" => "Ciudad de Oakland",
+	"Departments" => "Departamentos",
+	"News and Updates" => "Noticias y Actualizaciones",
+	"Services and Resources" => "Servicios y Recursos",
+	"Elected Officials" => "Oficiales Electos",
+	"Initiatives" => "Iniciativas",
+	"Reports" => "Informes",
+	"Forms" => "Formas",
+	"Address" => "Dirección",
+	"Email" => "Email",
+	"Phone Numbers" => "Números de Teléfono",
+	"Hours" => "Horas",
+	"Most Popular" => "Más popular",
+	"see all" => "ver todo",
+
+	## Homepage
+	"Welcome to Oakland" => "La Bienvenida a Oakland",
+	"The best place to find official services and information from the City of Oakland." => "El mejor lugar para encontrar los servicios oficiales e información de la ciudad de Oakland.",
+	"City of Oakland Mayor" => "Ciudad de Oakland Alcalde",
+	"Other News" => "Otras Noticias",
+	
+
+	## Global Nagivation
+	"This site is a work in progress." => "Este sitio es un trabajo en progreso.",
+	"Official Website" => "Página web oficial",
+	"Learn More" => "Aprende más",
+	"How can we help?" => "¿Cómo podemos ayudar?",
+
+	## Footer
+	"Oakland Government" => "Gobierno Oakland",
+	"Privacy Policy" => "Política de privacidad",
+	"Terms of Use" => "Términos de Uso",
+
+	## Sitewide Alert
+	"Read more" => "Lee mas",
+
+	## Search Results Page
+	"Search Results" => "Resultados de la búsqueda",
+	"pages match your search for" => "páginas corresponden a su búsqueda de",
+	"Top Hit" => "Top Hit",
+	"Your search for" => "Su búsqueda de",
+	"didn’t return any results." => "no arrojó ningún resultado.",
+	
+
+	## 404 Page
+	"Page Not Found" => "Página no encontrada",
+	"We apologize, the page you were looking has either been deleted or moved." => "Nos disculpamos, la página que busca ha sido o bien eliminado o movido.",
+	"Try returning to the" => "Intenta volver a la",
+	"homepage" => "página principal",
+	"or searching for what you need." => "o la búsqueda de lo que necesita.",
+
+	## Department Pages
+	"Our Management" => "Nuestra Gestión",
+	"Contact Us" => "Contáctenos",
+
+	## Form Pages
+	"Download the Form" => "Descargar el Formulario",
+
+	## Initiative Pages
+	"City Initiative" => "Iniciativa de la Ciudad",
+
+	## Press Release Pages
+	"Media contacts" => "contactos para los medios",
+	"Download this press release as a PDF" => "Descargar esta noticia en formato PDF",
+
+
+	## Elected Offical Pages
+	"Priorities" => "prioridades",
+	"Get in Touch" => "Estar en contacto",
+	"About" => "Acerca de",
+
+	## Report pages
+	"Published on" => "Publicado en",
+	"Download the Report" => "Descargar el Informe",
+
+	## Topic Pages
+	"This section is currently empty." => "Esta sección está actualmente vacía."
+
+
 )
 
 ?>

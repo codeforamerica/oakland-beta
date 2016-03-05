0b18b50ddb3bdfba73fc116bdb455f2122bbeb52
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2016-03-04 16:06:25 -0800

    added static chinese translations

--- craft/translations/zh.php
+++ craft/translations/zh.php
@@ -1,7 +1,87 @@
 <?php
 
 return array(
-	"Welcome to Oakland" => "歡迎來到奧克蘭"
+	## General
+	"Oakland" => "奥克兰",
+	"City of Oakland" => "Ciudad de Oakland",
+	"Departments" => "部门",
+	"News and Updates" => "新闻与更新",
+	"Services and Resources" => "服务和资源",
+	"Elected Officials" => "当选官员",
+	"Initiatives" => "倡议",
+	"Reports" => "报告",
+	"Forms" => "形式",
+	"Address" => "地址",
+	"Email" => "电子邮件",
+	"Phone Numbers" => "电话号码",
+	"Hours" => "小时",
+	"Most Popular" => "最受欢迎",
+	"see all" => "查看全部",
+
+	## Homepage
+	"Welcome to Oakland" => "欢迎来到奥克兰",
+	"The best place to find official services and information from the City of Oakland." => "最好的地方，从城市奥克兰找到官方服务和信息。",
+	"City of Oakland Mayor" => "奥克兰市长",
+	"Other News" => "其他新闻",
+	
+
+	## Global Nagivation
+	"This site is a work in progress." => "这个网站是一项正在进行的工作。",
+	"Official Website" => "官方网站",
+	"Learn More" => "学到更多",
+	"How can we help?" => "我们怎样才能帮助？",
+
+	## Footer
+	"Oakland Government" => "奥克兰政府",
+	"Privacy Policy" => "隐私政策",
+	"Terms of Use" => "使用条款",
+
+	## Sitewide Alert
+	"Read more" => "阅读更多",
+
+	## Search Results Page
+	"Search Results" => "搜索结果",
+	"pages match your search for" => "网页符合您搜索",
+	"Top Hit" => "顶部命中",
+	"Your search for" => "您的搜索",
+	"didn’t return any results." => "不返回任何结果。",
+	
+
+	## 404 Page
+	"Page Not Found" => "找不到网页",
+	"We apologize, the page you were looking has either been deleted or moved." => "我们道歉，你要找的页面已被删除或移动。",
+	"Try returning to the" => "尝试返回",
+	"homepage" => "主页",
+	"or searching for what you need." => "或者寻找你所需要的。",
+
+	## Department Pages
+	"Our Management" => "我们的管理",
+	"Contact Us" => "联系我们",
+
+	## Form Pages
+	"Download the Form" => "下载表格",
+
+	## Initiative Pages
+	"City Initiative" => "市行动",
+
+	## Press Release Pages
+	"Media contacts" => "媒体联系人",
+	"Download this press release as a PDF" => "下载本新闻稿为PDF",
+
+
+	## Elected Offical Pages
+	"Priorities" => "优先级",
+	"Get in Touch" => "保持联系",
+	"About" => "关于",
+
+	## Report pages
+	"Published on" => "发表于",
+	"Download the Report" => "下载报告",
+
+	## Topic Pages
+	"This section is currently empty." => "这部分是空的。"
+
+
 )
 
 ?>

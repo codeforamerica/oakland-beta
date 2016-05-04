4c006518686bd9a11075820893d17166704d2e4f
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2016-05-03 17:41:08 -0700

    working on events

--- public/resources/js/filterbox.js
+++ public/resources/js/filterbox.js
@@ -1,5 +1,12 @@
 $('document').ready(function() {
 	$('.filterbox-filter-label').click(function(e) {
-		$(this).parent().toggleClass('filterbox-filter-is-open');
+		var currFilter = $(this).parent();
+		if($(currFilter).hasClass('filterbox-filter-is-open')) {
+			$(currFilter).removeClass('filterbox-filter-is-open');
+		}
+		else {
+			$('.filterbox-filter').removeClass('filterbox-filter-is-open');
+			$(currFilter).addClass('filterbox-filter-is-open');
+		}
 	})
 })

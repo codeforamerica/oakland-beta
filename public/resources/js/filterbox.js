6725c999b016f58041e8b2d448a42d0b75dbe534
Norris Hung <norrishung@Norriss-MacBook-Pro.local>
2016-05-05 16:16:30 -0700

    added news filter/search

--- public/resources/js/filterbox.js
+++ public/resources/js/filterbox.js
@@ -1,12 +1,21 @@
 $('document').ready(function() {
-	$('.filterbox-filter-label').click(function(e) {
-		var currFilter = $(this).parent();
-		if($(currFilter).hasClass('filterbox-filter-is-open')) {
-			$(currFilter).removeClass('filterbox-filter-is-open');
+
+	$(document).on('click', function(e) {
+		if($(e.target).hasClass('filterbox-filter-label')) {
+			var currFilter = $(e.target).parent();
+			console.log(!$(currFilter).hasClass('filterbox-filter-is-open'));
+			if(!$(currFilter).hasClass('filterbox-filter-is-open')) {
+				$('.filterbox-filter').removeClass('filterbox-filter-is-open');
+				$(currFilter).addClass('filterbox-filter-is-open');
+			}
+			else {
+				$(currFilter).removeClass('filterbox-filter-is-open');
+			}
 		}
-		else {
-			$('.filterbox-filter').removeClass('filterbox-filter-is-open');
-			$(currFilter).addClass('filterbox-filter-is-open');
+
+		else if(!$(e.target).closest('.filterbox-drawer').length) {
+			$('.filterbox-filter-is-open').removeClass('filterbox-filter-is-open');
 		}
 	})
+
 })
